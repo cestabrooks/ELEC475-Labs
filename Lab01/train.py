7c2b76887a73740842f3feb40ae96ac11fa6d57f
ConnorMassey01 <cmassey646@gmail.com>
2023-09-13 18:16:22 -0400

    Flatten the data into 1x784 tensor

--- Lab01/train.py
+++ Lab01/train.py
@@ -1,4 +1,3 @@
-
 import sys
 
 import torch
@@ -18,50 +17,44 @@ def train(n_epochs, optimizer, model, loss_fn, train_loader, scheduler, device):
     losses_train = []
 
     # send model for use on M1 gpu AS WELL as imgs
-    model.to("mps")
+    model.to(device)
 
-    for epoch in range(1, n_epochs+1):
+    for epoch in range(1, n_epochs + 1):
         print('epoch ', epoch)
         loss_train = 0.0
-        count = 0
+        # iterate over each batch of data
         for imgs, labelOfnumWrittenInImg in train_loader:
-            count = count + 1
-            if count > 10:
-                break
 
-            imgs = imgs.to(device=device)
+            imgs = imgs.to(device=device)  # move images to the correct device
+            # print("imgs size", imgs.size())
 
-            # # Flattening the images
-            # imgs = imgs.view(imgs.size(0), -1)
-            #iterate through each image and flatten it
-            #imgs = <batch size> of (28x28)
-            count = 0
-            for img in imgs:
-                print(img.size())
-                imgs[count] = torch.flatten(img)
-                print(img)
-                count = count + 1
-            # CANT JUST OVERRIDE OLD TENSOR NEED TO PREPROCESS BEFORE CALLING TRAIN
-            # AND MAKE A NEW TENSOR WITH FLATTEN IMAGES 1X784
+            # Flattening the images to size (2048, 1, 784)
+            flattendImgs = torch.flatten(imgs, start_dim=2)  # flatten the images to the correct size
+            # Ensure that the flattened images are on the correct device
+            flattendImgs.to(device=device)
+            # print the flatten tensors shape
+            # print("Flatten tensor size: ", flattendImgs.size())
 
-            outputs = model(imgs)
-            loss = loss_fn(outputs, imgs)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            loss_train += loss.item()
+            outputs = model(flattendImgs)                   # forward propagation
+            loss = loss_fn(outputs, flattendImgs)           # calculate loss
+            optimizer.zero_grad()                           # reset optimizer gradients to zero
+            loss.backward()                                 # calculate the loss gradients
+            optimizer.step()                                # iterate the optimization, based on the loss gradients
+            loss_train += loss.item()                       # update the value of losses
 
-        scheduler.step(loss_train)
+        scheduler.step(loss_train)                          # update optimization hyperparameters
 
-        losses_train += [loss_train/len(train_loader)]
+        losses_train += [loss_train / len(train_loader)]    # update value of losses
 
-        print('{} Epoch {}, Training loss {}'.format( datetime.datetime.now(), epoch, loss_train/len(train_loader)))
+        print('{} Epoch {}, Training loss {}'.format(datetime.datetime.now(), epoch, loss_train / len(train_loader)))
+
 
 def getMNISTDataset():
     train_transform = transforms.Compose([transforms.ToTensor()])
     train_set = datasets.MNIST('./data/mnist', train=True, download=True, transform=train_transform)
 
     return train_set
+
 
 if __name__ == '__main__':
     # Get arguments from command line
@@ -78,9 +71,9 @@ if __name__ == '__main__':
 
     # data loader
     train_loader = torch.utils.data.DataLoader(train_set, b, shuffle=False)
-
-    scheduler = torch.optim.lr_scheduler.StepLR(torch.optim.Adam(model.parameters(), lr=0.001),  step_size=5, gamma=0.5)
-    train(e, torch.optim.Adam(model.parameters(), lr=0.001), model, nn.MSELoss(), train_loader, scheduler, "mps")
-
+    # scheduler
+    scheduler = torch.optim.lr_scheduler.StepLR(torch.optim.Adam(model.parameters(), lr=0.001), step_size=5, gamma=0.5)
+    # train the model
+    train(e, torch.optim.Adam(model.parameters(), lr=0.001), model, nn.MSELoss(), train_loader, scheduler, "cpu")
     # args model, shape of input data
-    summary(model, (1, 28, 28))
+    summary(model, (1, 784), batch_size=b, device="cpu")
