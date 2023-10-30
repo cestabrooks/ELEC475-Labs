f32f0f41beec71acddbd4cf33d53b2e28ca138ec
ConnorMassey01 <cmassey646@gmail.com>
2023-10-30 17:08:06 -0400

    Used cross entropy loss, different optimizer, and smaller batch size

--- Lab03/train.py
+++ Lab03/train.py
@@ -7,20 +7,28 @@ from torchsummary import summary
 import matplotlib.pyplot as plt
 import datetime
 
+
 def getCIFAR_10_Dataset():
-    transform = transforms.Compose([transforms.ToTensor()])
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.225, 0.225, 0.225])
+    transform = transforms.Compose([transforms.ToTensor(), normalize])
     train_set = datasets.CIFAR10('./data/cifar_10', train=True, download=True, transform=transform)
     return train_set
 
+
 def getCIFAR_100_Dataset():
-    transform = transforms.Compose([transforms.ToTensor()])
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.225, 0.225, 0.225])
+    transform = transforms.Compose([transforms.ToTensor(), normalize])
     train_set = datasets.CIFAR100('./data/cifar_10', train=True, download=True, transform=transform)
     return train_set
+
 
 def train(model, optimizer, n_epochs, loss_fn, data_laoder, device, plot_file, save_file):
     print("Starting training...")
     model.train()
     losses_train = []
+    accuracy_list = []
 
     # send model for use on M1 gpu AS WELL as imgs
     model.to(device)
@@ -28,28 +36,32 @@ def train(model, optimizer, n_epochs, loss_fn, data_laoder, device, plot_file, s
     for epoch in range(1, n_epochs + 1):
         print('epoch ', epoch)
         loss_train = 0.0
+        accuracy = 0.0
         # iterate over each batch of data
         for imgs, labels in data_laoder:
-            # move images to the correct device
+            # move images and labels to the correct device
             imgs = imgs.to(device=device)
-            # Create a one_hot tensor of the output labels
-            one_hot = torch.nn.functional.one_hot(labels, 10).to(torch.float32)
-            one_hot = one_hot.to(device=device)
+            labels = labels.to(device=device)
             # forward propagation
             outputs = model(imgs)
             # calculate loss
-            loss = loss_fn(outputs, one_hot)
+            loss = loss_fn(outputs, labels)
+            # calculate accuracy
+            predictions = torch.argmax(outputs, dim=1)
+            accuracy += ((predictions == labels).sum().item()/len(labels)) * 100
             # reset optimizer gradients to zero
             optimizer.zero_grad()
             # calculate the loss gradients
             loss.backward()
             # iterate the optimization, based on the loss gradients
-            optimizer.step()                                # iterate the optimization, based on the loss gradients
-            loss_train += loss.item()                       # update the value of losses
+            optimizer.step()  # iterate the optimization, based on the loss gradients
+            loss_train += loss.item()  # update the value of losses
 
-        losses_train += [loss_train / len(data_loader)]    # update value of losses
-
-        print('{} Epoch {}, Training loss {}'.format(datetime.datetime.now(), epoch, loss_train / len(data_loader)))
+        losses_train += [loss_train / len(data_loader)]  # update value of losses
+        accuracy_list += [accuracy / len(data_loader)]
+        print('{} Epoch {}, Training loss {} Accuracy {}'.format(datetime.datetime.now(), epoch,
+                                                                 loss_train / len(data_loader),
+                                                                 accuracy / len(data_loader)))
 
     # plot the losses_train
     if save_file != None:
@@ -62,6 +74,7 @@ def train(model, optimizer, n_epochs, loss_fn, data_laoder, device, plot_file, s
         plt.ylabel('loss')
         plt.legend(loc=1)
         plt.savefig(plot_file)
+
 
 if __name__ == "__main__":
     # Get arguments from command line
@@ -96,7 +109,9 @@ if __name__ == "__main__":
     data_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
     print("Dataset size: ", len(train_set))
 
-    train(model, torch.optim.Adam(model.parameters(), lr=0.001), 20, torch.nn.MSELoss(), data_loader, device, loss_plot, save_pth)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer_SGD = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
+
+    train(model, optimizer_SGD, n_epoch, torch.nn.CrossEntropyLoss(), data_loader, device, loss_plot, save_pth)
 
     summary(model, (3, 32, 32), batch_size=batch_size, device=device)
-
