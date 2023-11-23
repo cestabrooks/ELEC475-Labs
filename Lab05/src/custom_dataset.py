efe8693df6a2e294f2aa8ca3474f827ed957ebcb
Cameron Estabrooks <camestabrooks@yahoo.ca>
2023-11-23 13:24:00 -0500

    Lab 5: Initial code

---
+++ Lab05/src/custom_dataset.py
@@ -0,0 +1,32 @@
+import os
+from torch.utils.data import Dataset
+from PIL import Image, ImageFile
+import json
+
+
+class custom_dataset(Dataset):
+    def __init__(self, dir, transform=None):
+        super().__init__()
+        Image.MAX_IMAGE_PIXELS = None
+        ImageFile.LOAD_TRUNCATED_IMAGES = True
+        self.transform = transform
+
+        self.image_files = []
+        for file_name in os.listdir(dir):
+            if file_name.endswith(".png"):
+                self.image_files.append(dir + file_name)
+
+        self.labels = []
+        label_file = open(dir + "/labels.txt", "r")
+        for line in label_file:
+            self.labels.append(line.split(" ")[1])
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, index):
+        image = Image.open(self.image_files[index]).convert('RGB')
+        image_sample = self.transform(image)
+        # print('break 27: ', index, image, image_sample.shape)
+        return image_sample, int(self.labels[index])
+
